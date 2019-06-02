439685d325d332429e6dcbb995e26ba5667cc89d
lboudjella <lab1278@yahoo.fr>
2019-06-02 13:27:10 +0200

    version activation BDD activée OK, suite à retour arrière après echec authentification

--- app/src/main/java/com/cybersporttech/sportcoach/controller/MainActivity.java
+++ app/src/main/java/com/cybersporttech/sportcoach/controller/MainActivity.java
@@ -2,19 +2,26 @@ package com.cybersporttech.sportcoach.controller;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.view.View;
-
+import android.widget.Toast;
 import com.cybersporttech.sportcoach.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,75 +29,71 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference myRef = database.getReference("message");
 
     private TextView mGreetingText;
-    private EditText mNameInput;
-    private Button mPlayButton;
+    private ProgressBar mProgressBar;
+    private EditText mPassword;
+    private EditText mEmailInput;
     private Button mRegisterButton;
-
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            mGreetingText =  findViewById(R.id.activity_main_greeting_txt);
-            mNameInput = findViewById(R.id.activity_main_name_input);
-            mRegisterButton = findViewById(R.id.activity_register_team_btn);
-            mRegisterButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // The user just clicked
-                }
-            });
-            mPlayButton = findViewById(R.id.activity_main_play_btn);
-            mPlayButton.setEnabled(false);
-            mPlayButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // The user just clicked
-
-                }
-            });
-            mNameInput.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    mPlayButton.setEnabled(s.toString().length() != 0); //activation du bouton dès la saisie d'un caractère
-                    mRegisterButton.setEnabled(true); // activation du bouton pour pouvoir s'enregistrer
+    private Button mConnexionButton;
+    FirebaseAuth firebaseAuth;
 
 
-                }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingText = findViewById(R.id.activity_main_mGreetingText);
+        mEmailInput = findViewById(R.id.activity_main_email_input);
+        mPassword = findViewById(R.id.activity_main_password);
+        mProgressBar = findViewById(R.id.main_progressBar);
+        mRegisterButton = findViewById(R.id.activity_register_btn);
+        mConnexionButton = findViewById(R.id.activity_main_connexion_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        mRegisterButton.setEnabled(false);
 
-                @Override
-                public void afterTextChanged(Editable s) {
-                    setMessage();
-                }
+        mEmailInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            });
-            mPlayButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent menuActivity = new Intent(MainActivity.this, MenuActivity.class);
-                    startActivity(menuActivity);
-                    //Ce bouton permet à l'utilisateur d'arriver sur la page menu
+            }
 
-                }
-            });
-            mRegisterButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent registerActivity = new Intent(MainActivity.this, RegisterActivity.class);
-                    startActivity(registerActivity);
-                    //Ce bouton permet à l'utilisateur d'arriver sur la page d'enregistrement
-                }
-            });
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mRegisterButton.setEnabled (s.toString().length()!=1);//activation du bouton dès la saisie d'un caractère
 
-        }
-    public void setMessage ()
-    {
-        String str = mNameInput.getText().toString().trim();
-        myRef.setValue(str); // zone de saisie destinée à être enregistrée dans la bdd Firebase
-    }
-}
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+        mConnexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menuActivity = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(menuActivity);
+                //Ce bouton permet à l'utilisateur d'arriver sur la page menu
+
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+                setMessage();
+                Intent registerActivity = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+                //Ce bouton permet à l'utilisateur d'arriver sur la page d'enregistrement
+            }
+
+            public void setMessage() {
+                String str = mEmailInput.getText().toString().trim();
+                myRef.setValue(str); // zone de saisie destinée à être enregistrée dans la bdd Firebase
+            }
+
+
+        });
+    }}
--- app/src/main/java/com/cybersporttech/sportcoach/controller/RegisterActivity.java
+++ app/src/main/java/com/cybersporttech/sportcoach/controller/RegisterActivity.java
@@ -16,13 +16,14 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mmain_regisurname_input;
     private EditText mmain_registel_input;
     private EditText mmain_regisport_input;
-    private EditText mmain_regisclub__input;
+    private EditText mmain_regisclub_input;
     private EditText mmain_regiscateg_input;
     private EditText mmain_regisnumequipe_input;
     private EditText mmain_regismail_input;
+    private EditText mmain_regisPass_input;
     private Button mmain_regisplayer_btn;
     private Button mmain_regiscoach_btn;
-    private Button mmain_regisfiche_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +34,14 @@ public class RegisterActivity extends AppCompatActivity {
         mmain_regisurname_input = findViewById(R.id.activity_main_regisurname_input);
         mmain_registel_input = findViewById(R.id.activity_main_registel_input);
         mmain_regisport_input = findViewById(R.id.activity_main_regisport_input);
-        mmain_regisclub__input = findViewById(R.id.activity_main_regisclub__input);
+        mmain_regisclub_input = findViewById(R.id.activity_main_regisclub_input);
         mmain_regiscateg_input = findViewById(R.id.activity_main_regiscateg_input);
         mmain_regisnumequipe_input = findViewById(R.id.activity_main_regisnumequipe_input);
         mmain_regismail_input = findViewById(R.id.activity_main_regismail_input);
+        mmain_regisPass_input = findViewById(R.id.activity_main_regisPass_input);
         mmain_regisplayer_btn = findViewById(R.id.activity_main_regisplayer_btn);
         mmain_regiscoach_btn = findViewById(R.id.activity_main_regiscoach_btn);
-        mmain_regisfiche_btn = findViewById(R.id.activity_main_regisfiche_btn);
+
 
 
 
